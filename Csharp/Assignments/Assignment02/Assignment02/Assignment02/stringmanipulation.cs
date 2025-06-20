ca2b05f2d68a06062b429efccd3b65aba285b14a
Nandini Arjangi <nandiniar@infinite.com>
2025-06-20 11:31:35 +0530

    Assignment02

---
+++ Csharp/Assignments/Assignment02/Assignment02/Assignment02/stringmanipulation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace assignment2
+{
+    class stringmanipulation
+    {
+        static void Main()
+        {
+            Console.WriteLine(" length of string");
+            length();
+            Console.WriteLine("********");
+            Console.WriteLine("reverse a string");
+            reverse();
+            Console.WriteLine("********");
+            Console.WriteLine("Check if two strings are equal");
+            checkIfEqual();
+            Console.WriteLine("********");
+            Console.Read();
+        }
+
+        // 1. Write a program in C# to accept a word from the user and display the length of it.
+        static void length()
+        {
+            Console.WriteLine("Enter the word: ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"Length of string {s} is {s.Length}");
+        }
+
+        // 2. Write a program in C# to accept a word from the user and display the reverse of it.
+        static void reverse()
+        {
+            Console.WriteLine("Enter the word: ");
+            string s = Console.ReadLine();
+            Console.WriteLine($"Input word: {s}");
+            StringBuilder rev = new StringBuilder();
+            for (int i = s.Length - 1; i >= 0; i--)
+                rev.Append(s[i]);
+            Console.WriteLine($"Reversed word: {rev}");
+        }
+
+        // 3. Write a program in C# to accept two words from user and find out if they are same.
+        static void checkIfEqual()
+        {
+            Console.WriteLine("Enter the first word: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Enter the second word: ");
+            string b = Console.ReadLine();
+            if (a.Equals(b))
+                Console.WriteLine($"{a} & {b} are equal");
+            else
+                Console.WriteLine($"{a} & {b} are not equal");
+        }
+    }
+
+}
---
+++ Csharp/Assignments/Assignment02/Program1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Assignment02
+{
+    class Program1
+    {
+        public static void SwapTwoNumbers(int a,int b)
+        {
+            
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After swapping the first is:{0} and the second is: {1}" ,a, b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number1:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number2:");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Program1.SwapTwoNumbers(n,m);
+            Console.ReadLine();
+
+        }
+    }
+}
