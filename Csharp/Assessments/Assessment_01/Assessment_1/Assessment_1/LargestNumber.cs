6e542c5ce8b93a8cf17740261b4ce77009ef500d
Nandini Arjangi <nandiniar@infinite.com>
2025-06-25 17:54:37 +0530

    Assessment01

---
+++ Csharp/Assessments/Assessment_01/Assessment_1/Assessment_1/LargestNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Assessment_1
+{
+    class LargestNumber
+    {
+        public void Largest(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                Console.WriteLine("The Largest number is {0}", a);
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("The Largest number is {0}", b);
+            }
+            else
+            {
+                Console.WriteLine("The Largest number is {0}", c);
+            }
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Enter the 1st num: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the 2nd num: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 3rd num: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            LargestNumber ob = new LargestNumber();
+            ob.Largest(a, b, c);
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Csharp/Assessments/Assessment_01/Assessment_1/Assessment_1/Removecharacter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Assessment_1
+{
+    class RemoveCharacter
+    {
+        public static void Remove_Character(string s,int pos)
+        {
+            string result = " ";
+            if(pos<0 || pos >= s.Length)
+            {
+                Console.WriteLine("Invalid position");
+            }
+            else
+            {
+                result = s.Remove(pos, 1);
+            }
+            Console.WriteLine("After Removing Characters:{0}", result);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the String");
+            string s = Console.ReadLine();
+            Console.WriteLine("Enter the position to remove");
+            int pos = Convert.ToInt32(Console.ReadLine());
+            Remove_Character(s, pos);
+            Console.Read();
+
+        }
+    }
+}
