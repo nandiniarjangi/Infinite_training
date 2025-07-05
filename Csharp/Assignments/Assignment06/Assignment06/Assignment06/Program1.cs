81654b1964d4068ce0df35cb37fac53ed088c409
Nandini Arjangi <nandiniar@infinite.com>
2025-07-05 20:01:20 +0530

    Assignment06

---
+++ Csharp/Assignments/Assignment06/Assignment06/Assignment06/Program1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Indexers:
+
+//3.Create a class called Books with BookName and AuthorName as members. Instantiate the class through constructor and also write a method Display() to display the details. 
+
+//Create an Indexer of Books Object to store 5 books in a class called BookShelf.Using the indexer method assign values to the books and display the same.
+
+//Hint(use Aggregation/composition)
+
+namespace Assignment_6
+{
+    class Books
+    {
+        string BookName;
+        string AuthorName;
+        public Books(string BookName, string AuthorName)
+        {
+            this.BookName = BookName;
+            this.AuthorName = AuthorName;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Book: {BookName} Author: {AuthorName}");
+        }
+    }
+    class BookShelf
+    {
+        Books[] books;
+        public BookShelf(int n)
+        {
+            this.books = new Books[n];
+        }
+        public Books this[int i]
+        {
+            get { return books[i]; }
+            set { books[i] = value; }
+        }
+    }
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of books : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            BookShelf bookShelf = new BookShelf(n);
+            int i = 0;
+            for (; i < n; i++)
+            {
+                Console.Write("Enter the book name: ");
+                string bookName = Console.ReadLine();
+                Console.Write("Enter the author name: ");
+                string authorName = Console.ReadLine();
+                Books newBook = new Books(bookName, authorName);
+                bookShelf[i] = newBook;
+            }
+            i = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"Enter the index of the book between 0 and {n - 1} or  any character key to exit");
+                    if (!int.TryParse(Console.ReadLine(), out i))
+                        break;
+                    bookShelf[i].Display();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("********");
+            Console.ReadLine();
+        }
+    }
+}
